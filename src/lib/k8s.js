4fddd4574e8ec30de1ada04bb4c428800910c259
Shubham Rajvanshi <shubhamrajvanshi9@gmail.com>
2023-08-21 09:58:37 -0700

    Switched to upstream k8s library

--- src/lib/k8s.js
+++ src/lib/k8s.js
@@ -1,52 +1,22 @@
-var Client = require('node-kubernetes-client');
-var config = require('./config');
-var util = require("util");
+const k8s = require('@kubernetes/client-node');
 
-fs = require('fs');
 
-var readToken = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token');
-
-var client = new Client({
-  host: config.k8sROServiceAddress,
-  namespace: config.namespace,
-  protocol: 'https',
-  version: 'v1',
-  token: readToken
-});
-
-var getMongoPods = function getPods(done) {
-  client.pods.get(function (err, podResult) {
-    if (err) {
-      return done(err);
-    }
-    var pods = [];
-    for (var j in podResult) {
-      pods = pods.concat(podResult[j].items)
-    }
-    var labels = config.mongoPodLabelCollection;
-    var results = [];
-    for (var i in pods) {
-      var pod = pods[i];
-      if (podContainsLabels(pod, labels)) {
-        results.push(pod);
-      }
-    }
-
-    done(null, results);
-  });
-};
-
-var podContainsLabels = function podContainsLabels(pod, labels) {
-  if (!pod.metadata || !pod.metadata.labels) return false;
-
-  for (var i in labels) {
-    var kvp = labels[i];
-    if (!pod.metadata.labels[kvp.key] || pod.metadata.labels[kvp.key] != kvp.value) {
-      return false;
-    }
+var getMongoPods = async function getPods(done) {
+  const kc = new k8s.KubeConfig();
+  try {
+  kc.loadFromDefault();
+  // kc.loadFromFile("/Users/shubham/Downloads/srec.kubeconfig")
+  } catch(err) {
+    console.log("unable to load kube config")
+    return done(err)
   }
-
-  return true;
+  const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+  await k8sApi.listPodForAllNamespaces("","","","role=mongo").then((value)=> {
+    var pods = value.body.items;
+    done(null, pods);
+  }).catch((err) => {
+    console.log("unable to fetch pods")
+    done(err)})
 };
 
 module.exports = {
